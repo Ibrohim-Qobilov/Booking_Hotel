e5a774bbc0e87e7b0ba224a1667f085c9975b6f5
ibrokhim-kobilov <ibrokhim112@gmail.com>
2021-12-24 21:44:08 +0500

    added icons and fonts

---
+++ lib/core/constants/icons.dart
@@ -0,0 +1,22 @@
+class IconName{
+  // for bottom navigation bar
+  static String home = "assets/icons/home.svg";
+  static String homeActive = 'assets/icons/home_acrtive.svg';
+  static String search = "assets/icons/search.svg";
+  static String searchActive = "assets/icons/Search.svg";
+  static String notification = "assets/icons/bell.svg";
+  static String notificationActive = "assets/icons/bell_active.svg";
+  static String profile = "assets/icons/user.svg";
+  static String profileActive = "assets/icons/user_active.svg";
+
+  // for other locations
+
+  static String setting = "assets/icons/setting.svg";
+  static String promotions = "assets/icons/promotions.svg";
+  static String payment = "assets/icons/payment.svg";
+  static String filter = "assets/icons/filter.svg";
+  static String help = "assets/icons/help.svg";
+  static String yourFavorite = "assets/icons/your_favorite.svg";
+  static String favorite = "assets/icons/favorite.svg";
+
+}
