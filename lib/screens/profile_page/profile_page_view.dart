95ad81a1b42eb3083cc5e754305b1fa14c8b5ec0
ibrokhim-kobilov <ibrokhim112@gmail.com>
2021-12-28 22:09:51 +0500

    profile qismi boshlandi

--- lib/screens/profile_page/profile_page_view.dart
+++ lib/screens/profile_page/profile_page_view.dart
@@ -1,13 +1,25 @@
-import 'package:flutter/material.dart';
+
+
+import 'package:booking_hotel/core/components/export_page.dart';
 import './profile_page_view_model.dart';
-  
+
 class ProfilePageView extends ProfilePageViewModel {
-    
   @override
   Widget build(BuildContext context) {
-    
     // Replace this with your build function
-    return const Scaffold(body: Center(child: Text("Profile"),),);
+    return Scaffold(
+      backgroundColor: MainColor.kWhite,
+      body: Center(
+          child: Column(
+        children: [
+          SizedBox(
+            height: getProportionateScreenHeight(40.0),
+          ),
+          profileMethod("https://images.unsplash.com/photo-1640558378987-74741517e220?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxMjF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", "Nimadur ism")
+        ],
+      )),
+    );
   }
-}
 
+  
+}
--- lib/screens/profile_page/profile_page_view_model.dart
+++ lib/screens/profile_page/profile_page_view_model.dart
@@ -1,6 +1,40 @@
-import 'package:flutter/material.dart';
-import './profile_page.dart';
+import 'package:booking_hotel/core/components/export_page.dart';
 
 abstract class ProfilePageViewModel extends State<ProfilePage> {
+  Container profileMethod(profileImage, profileText) {
+    return Container(
+          height: getProportionateScreenHeight(70.0),
+          decoration: BoxDecoration(
+            color: MainColor.kWhite,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.1),
+                spreadRadius: 2,
+                blurRadius: 2,
+                offset: const Offset(0, 1),
+              ),
+            ],
+          ),
+          child: ListTile(
+            leading: Padding(
+              padding:  EdgeInsets.only(top: getProportionateScreenHeight(5.0)),
+              child: Container(
+                height: getProportionateScreenHeight(55),
+                width: getProportionateScreenWidth(55),
+                decoration:  BoxDecoration(
+                  borderRadius: BorderRadius.circular(70),
+                  image:  DecorationImage(
+                    fit: BoxFit.cover,
+                    image:  NetworkImage(
+                      profileImage,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            title: Text(profileText,style: TextStyle(fontSize: getProportionateScreenWidth(20),color: MainColor.kBlacText),),
+          ),
+        );
+  }
   // Add your state and logic here
 }
