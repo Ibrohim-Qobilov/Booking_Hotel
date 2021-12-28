e0b19af9b0e3d54a3a3ef3d86822ccff7ec8ca2b
ibrokhim-kobilov <ibrokhim112@gmail.com>
2021-12-28 23:38:00 +0500

    notification page started

--- lib/screens/notifications_page/notifications_page_view.dart
+++ lib/screens/notifications_page/notifications_page_view.dart
@@ -1,13 +1,32 @@
+import 'package:booking_hotel/core/components/export_page.dart';
 import 'package:flutter/material.dart';
 import './notifications_page_view_model.dart';
-  
+
 class NotificationsPageView extends NotificationsPageViewModel {
-    
   @override
   Widget build(BuildContext context) {
-    
     // Replace this with your build function
-    return const Scaffold(body: Center(child: Text("notification"),),);
+    return Scaffold(
+      backgroundColor: MainColor.kWhite,
+      body: Center(
+          child: Column(
+        children: [
+          // SizedBox(height: getProportionateScreenHeight(30.0)),
+          Container(
+            height: getProportionateScreenHeight(300),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(
+                    "https://images.unsplash.com/photo-1564501049412-61c2a3083791?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aG90ZWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"),
+              ),
+            ),
+            child: Column(
+              children: [],
+            ),
+          ),
+        ],
+      )),
+    );
   }
 }
-
