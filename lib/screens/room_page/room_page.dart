fb0fa1e766d31d325a54873d478c1cde8b31c26a
ibrokhim-kobilov <ibrokhim112@gmail.com>
2021-12-29 18:20:22 +0500

    added room page

---
+++ lib/screens/room_page/room_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import './room_page_view.dart';
+
+class RoomPage extends StatefulWidget {
+  
+  @override
+  RoomPageView createState() => new RoomPageView();
+}
+  
---
+++ lib/screens/room_page/room_page_view_model.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import './room_page.dart';
+
+abstract class RoomPageViewModel extends State<RoomPage> {
+  // Add your state and logic here
+}
